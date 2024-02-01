6e55d11163fec568ec01700a4299193492f8eff6
Andrew Gabros <andrewgabros@Read-a-macbook.local>
2024-02-01 17:25:24 -0500

    modified permutation test loop, added timers and output for improving efficiency

--- pacHist.py
+++ pacHist.py
@@ -6,22 +6,23 @@ import json
 from tqdm import tqdm
 import h5py
 import multiprocessing
-from functools import partial
-import cProfile
+from datetime import datetime
 
 ## FUNCTIONS
 
 def load_data(root, subject):
+    startLoad = datetime.now()
     data = pd.read_csv(f'{root}/{subject}/{subject}_data.csv', header=None)
     with open(f'{root}/{subject}/{subject}_param.json', 'r') as file:
         param = json.load(file)
     if len(param['channel_id_labels']) == data.shape[1]:    
-        print(f'Loaded {data.shape[0]} datapoints and {len(param.keys())} parameters for {data.shape[1]} channels')
+        print(f'Loaded {data.shape[0]} datapoints and {len(param.keys())} parameters for {data.shape[1]} channels. Time elapsed: {datetime.now() - startLoad}')
     else:
         print('Error loading data')
     return data, param
 
 def extract_params(param, datashape):
+    startParam = datetime.now()
     Nch = datashape[1]
     sample_rate = param['sample_rate']
     tperm = param['tperm']
@@ -38,23 +39,51 @@ def extract_params(param, datashape):
     edges = np.arange(-np.pi, np.pi+param['delta'], param['delta'])
     x = edges[:-1] + param['delta'] / 2
     Nx = len(x)
+    print(f"Loaded parameters. Time elapsed: {datetime.now() - startParam}")
     return Nch, sample_rate, tperm, Nperm, sbin, sstp, rT, Nt, t, rP, rA, NP, NA, edges, x, Nx
 
-def process_channel(ich, data, param):
-    Nch, sample_rate, tperm, Nperm, sbin, sstp, rT, Nt, t, rP, rA, NP, NA, edges, x, Nx = extract_params(param, data.shape)
+def perm_test(inputs):
+    startUnpack = datetime.now()
+    tperm, A, rT, sbin, sample_rate, Nt, Nx, Pbin, log_Nx = inputs
+    print(f"Unpacking inputs complete. Time elapsed: {datetime.now()-startUnpack}")
+    MIperm = np.zeros((1,Nt));
+    startrand = datetime.now()
+    tshift = tperm[0] + np.diff(tperm)[0] * np.random.rand()
+    print(f"Randomized tshift complete. Time elapsed: {datetime.now()-startrand}")
+    startround = datetime.now()
+    nshift = round(tshift * sample_rate)
+    print(f"Rounded tshift complete. Time elapsed: {datetime.now()-startround}")
+    startashift = datetime.now()
+    Ashift = np.roll(A, nshift)
+    print(f"Amplitude shift complete. Time elapsed: {datetime.now()-startashift}")
+    startloop = datetime.now()
+    for iT in range(Nt):
+        t1 = rT[iT]
+        t2 = rT[iT] + sbin
+        cA = Ashift[t1:t2]
+        cAm = np.array([np.mean(cA[Pbin[:, iT] == jj]) for jj in range(1, Nx + 1)])
+        cAm_sum = np.sum(cAm)
+        if cAm_sum > 0:
+            cAm /= cAm_sum
+            cAm[cAm == 0] = 1e-10
+        MIperm[0,iT] = (log_Nx + np.sum(cAm * np.log(cAm))) / log_Nx
+    print(f"Time loop complete. Time elapsed: {datetime.now()-startloop}")
+    return MIperm
 
+def process_channel(ich, data_numpy, param, shape):
+    Nch, sample_rate, tperm, Nperm, sbin, sstp, rT, Nt, t, rP, rA, NP, NA, edges, x, Nx = extract_params(param, shape)
     tmi = np.zeros((NP, NA, Nt))
     tmip = np.zeros((NP, NA, Nt))
-
-    data_numpy = data[ich].to_numpy()
     log_Nx = np.log(Nx)
-
-    for iP in tqdm(range(NP-15), desc=f'Channel {ich} Phase Progress'):
+    for iP in range(NP):
+        startPhase = datetime.now()
         bP, aP = butter(2, rP[iP, :] / (sample_rate / 2), btype='band')
         P = np.angle(hilbert(filtfilt(bP, aP, data_numpy)))
         Pbin = np.transpose(np.vstack([np.digitize(P[t1:t1+sbin], edges) for t1 in rT]))
-            
-        for iA in range(NA-15):
+        print(f"Phase binning completed. Time elapsed: {datetime.now() - startPhase}")
+     
+        for iA in range(NA):
+            startAmp = datetime.now()
             bA, aA = butter(2, rA[iA, :] / (sample_rate / 2), btype='band')
             A = np.abs(hilbert(filtfilt(bA, aA, data_numpy)))
             
@@ -66,24 +95,23 @@ def process_channel(ich, data, param):
                 cAm /= np.sum(cAm)
                 cAm[cAm == 0] = 1e-10
                 tmi[iP, iA, iT] = (log_Nx + np.sum(cAm * np.log(cAm))) / log_Nx
-                
+            print(f"Amplitude binning completed. Time elapsed: {datetime.now() - startAmp}")
+                  
             if Nperm > 1:
+                Nperm = 5
+                startInputs = datetime.now()
+                inputs = [(tperm, A, rT, sbin, sample_rate, Nt, Nx, Pbin, log_Nx) for _ in range(Nperm)]
+                print(f"Assigning permtest inputs completed. Time elapsed: {datetime.now() - startInputs}")
+                startptest = datetime.now()
                 MIperm = np.zeros((Nperm, Nt))
-                for iperm in range(Nperm-195):
-                    tshift = tperm[0] + np.diff(tperm)[0] * np.random.rand()
-                    nshift = round(tshift * sample_rate)
-                    Ashift = np.roll(A, nshift)
-                    
-                    for iT in range(Nt):
-                        t1 = rT[iT]
-                        t2 = rT[iT] + sbin
-                        cA = Ashift[t1:t2]
-                        cAm = np.zeros(Nx)
-                        for jj in range(Nx):
-                            cAm[jj] = np.mean(cA[Pbin[:, iT] == jj+1])
-                        cAm /= np.sum(cAm)
-                        cAm[cAm == 0] = 1e-10
-                        MIperm[iperm, iT] = (log_Nx + np.sum(cAm * np.log(cAm))) / log_Nx
+                for ipm in range(Nperm):
+                      MIperm[ipm] = perm_test(inputs[ipm])
+                print(f"Permutation test complete. Time elapsed: {datetime.now() - startInputs}")
+                # with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+                #     partial_perms = pool.map(perm_test, inputs)
+                # MIperm = np.zeros((Nperm, Nt))
+                # for iperm, partial_perm in enumerate(partial_perms):
+                #     MIperm[iperm] = partial_perm
                 n = np.sum(MIperm >= np.ones((Nperm, 1)) * np.squeeze(tmi[iP, iA, :]), axis=0)
                 tmip[iP, iA, :] = (n + 1) / (Nperm + 1)
     PACmi_partial = np.concatenate([tmi[..., np.newaxis],tmip[..., np.newaxis]],axis=3)
@@ -108,20 +136,20 @@ def main():
     data, param = load_data(root, subject)
     Nch, sample_rate, tperm, Nperm, sbin, sstp, rT, Nt, t, rP, rA, NP, NA, edges, x, Nx = extract_params(param, data.shape)
     
-    # Set up and execute multiprocessing
-    process_with_args = partial(process_channel, data=data, param=param)
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    partial_results = list(tqdm(pool.imap(process_with_args, range(Nch)), total=Nch, leave=True)) # local
-    # partial_results = pool.map(process_with_args, range(Nch)) # HPC
-    pool.close()
-    pool.join()
-
     # Combine partial results
     PACmi = np.zeros((Nch, NP, NA, Nt, 2))
-    for ich, partial_result in enumerate(partial_results):
-        PACmi[ich] = partial_result
+    with open("progress_log.txt", "w") as log_file:
+        log_file.write(f"Accessing {multiprocessing.cpu_count()} CPUs\n")
+        log_file.flush() 
+        for ich in range(Nch):
+            log_file.write(f"Processing channel {ich+1}\n")
+            log_file.flush()    
+            start = datetime.now()
+            PACmi[ich] = process_channel(ich, data[ich].to_numpy(), param, data.shape)
+            log_file.write(f"Channel {ich+1} processing complete. Time elapsed: {datetime.now() - start} \n \n")
+            log_file.flush() 
 
-    save_results(PACmi, root, subject)
+    # save_results(PACmi, root, subject)
 
 if __name__ == "__main__":
-    cProfile.run(main())
+    main()
