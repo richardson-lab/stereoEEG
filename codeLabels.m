6f9e365a6c918fa5c1062db4f4a1c942af0ee106
Andrew Gabros <andrew.gabros@pennmedicine.upenn.edu>
2024-02-21 13:42:58 -0500

    generates map of similar channel location labels

--- codeLabels.m
+++ codeLabels.m
@@ -1,4 +1,3 @@
-% function codeLabels()
 % Generates common labeling scheme for all channel labels within seeg
 % dataset
 % AG 2/24
@@ -11,28 +10,63 @@ for ifile = 1:length(datfile)
     load(fullfile(datfile(ifile).folder,datfile(ifile).name),'PACparam');
     disp(datfile(ifile).name);
     nch(ifile) = size(PACparam.channel_labels,1);
-    pacchannels{ifile} = PACparam.channel_labels(:,2);
+    pacchannels{ifile} = PACparam.channel_labels;
 end
+save('ChannelLabels.mat','pacchannels');
 
 ulabels = '';
 for ip = 1:length(pacchannels)
-    ulabels = [ulabels; unique(pacchannels{ip})];
-
+    ulabels = [ulabels; unique(pacchannels{ip}(:,2))];
 end
 uniquelabels = unique(ulabels);
 %%
 % List of substrings to remove
-substringsToRemove = {'right', 'left', 'ctx', 'lh', 'rh', 'cortex', 'gyrus', '-', '\s+'};
+substrings_to_remove = {'right', 'left', 'ctx', 'lh', 'rh', 'cortex', 'gyrus', '-'};
 
-% Remove specified substrings and all whitespaces
-normalized_labels = cellfun(@(s) removeSpecifiedSubstrings(lower(s), substringsToRemove), uniquelabels, 'UniformOutput', false);
+% Normalize labels - lowercase, remove substrings, hyphens and whitespace
+normalized_labels = cellfun(@(s) normalizeLabels(lower(s), substrings_to_remove), uniquelabels, 'UniformOutput', false);
 
-function outputStr = removeSpecifiedSubstrings(inputStr, substringsToRemove)
+intlabels = groupSimilarStrings(normalized_labels);
+
+map = cell(max(intlabels),1);
+for i = 1:length(map)
+    map{i} = uniquelabels(intlabels==i);
+end
+
+save('ChannelLabelsMap.mat','map');
+
+%% FUNCTIONS %%
+
+function outputStr = normalizeLabels(inputStr, substrings_to_remove)
     % Remove each specified substring
-    for i = 1:length(substringsToRemove)
-        inputStr = regexprep(inputStr, substringsToRemove{i}, ' ', 'ignorecase');
+    for i = 1:length(substrings_to_remove)
+        inputStr = erase(inputStr, substrings_to_remove{i});
     end
-    outputStr = inputStr;
-    % % Remove all whitespace
-    % outputStr = regexprep(inputStr, '\s+', '');
+
+    % Remove all whitespace
+    outputStr = regexprep(inputStr, '\s+', '');
+end
+
+function labels = groupSimilarStrings(strings)
+    n = numel(strings);
+    labels = zeros(n, 1);
+    groupCount = 0;
+    normalizedStrings = lower(strings);
+    for i = 1:n
+        if labels(i) == 0
+            groupCount = groupCount + 1; % New group
+            labels(i) = groupCount;
+        end
+        
+        for j = i+1:n
+            if labels(j) == 0 && areSimilar(normalizedStrings{i}, normalizedStrings{j})
+                labels(j) = labels(i); % Assign to the same group
+            end
+        end
+    end
+end
+
+function isSimilar = areSimilar(str1, str2)
+    % Define a simple criterion for similarity
+    isSimilar = contains(str1, str2) || contains(str2, str1);
 end
